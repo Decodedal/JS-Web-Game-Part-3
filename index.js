2004ea8a962ffa31db63183979a316c1a147763f
Decodedal <Palumbodallas@gmail.com>
2022-06-16 12:39:28 -0400

    completed

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function move(image,left,bottom){
-    image.style.position="fixed"
-    image.style.left = left +'px'
-    image.style.bottom = bottom +'px'
+function move(image){
+    image.style.position = 'fixed'
+    
+    function moveToCoordinates(left, bottom){
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
+
+    return {
+        to: moveToCoordinates
+    }
 }
+
 
 function newImage(url){
     let image = document.createElement('img')
@@ -10,18 +18,19 @@ function newImage(url){
     document.body.append(image)
     return image
 }
+move(newImage('assets/green-character.gif')).to(100,350)
+// let greenCharacter = newImage('assets/green-character.gif')
+// move(greenCharacter,100, 350);
+move(newImage('assets/tree.png')).to(200,450)
+// newImage('assets/tree.png', 200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
 
-let greenCharacter = newImage('assets/green-character.gif')
-move(greenCharacter,100, 350);
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
 
-
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+function newItem(url){
+    let item = newImage(url)
     item.addEventListener('click', () => {
         item.remove()
         let inventoryItem = document.createElement('img')
@@ -31,9 +40,9 @@ function newItem(url, left, bottom){
     return item
 }
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to( 165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
 
 function newInventory(){
     let inventory = document.createElement('div')
